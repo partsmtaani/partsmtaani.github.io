b4a3e144cba3d0602f7a527d5c27d02643cd4291
malmalo <68249331+malmalo@users.noreply.github.com>
2026-01-15 12:08:36 +0300

    whole site from host

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+/* Description: Custom JS file */
+
+(function($) {
+    "use strict";
+	
+	/* Rotating Text - Morphtext */
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 1200,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+	});
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+})(jQuery);
